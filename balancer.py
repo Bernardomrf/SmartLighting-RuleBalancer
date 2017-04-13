11eb1d43a95ee1abe51172b8f4994e8a6e226cdb
Bernardo Ferreira <bernardomrferreira@ua.pt>
2017-04-13 21:26:42 +0100

    Balancer now saves topics list of each device

--- balancer.py
+++ balancer.py
@@ -2,18 +2,44 @@
 import paho.mqtt.client as mqtt
 import paho.mqtt.publish as publish
 import paho.mqtt.subscribe as subscribe
+import json
+import re
+
+topics_list = {}
+
 
 def on_message(client, userdata, msg):
     if msg.topic == '/SM/devconfig':
         publish.single(msg.topic, payload=msg.payload, qos=0, retain=False,
-        hostname="gateway-pi2.local", port=1883, client_id="", keepalive=60,
+        hostname="sonata5.local", port=1883, client_id="", keepalive=60,
         will=None, auth=None, tls=None, protocol=mqtt.MQTTv311)
-    else:
+
+    elif "/SM/devices/" not in msg.topic:
         publish.single(msg.topic, payload=msg.payload, qos=0, retain=False,
         hostname="gateway-pi.local", port=1883, client_id="", keepalive=60,
         will=None, auth=None, tls=None, protocol=mqtt.MQTTv311)
+        print(msg.topic)
+        for regex in topics_list:
+            if regex.match(msg.topic):
+                print(topics_list[regex])
+    else:
 
-subscribe.callback(on_message, ["/SM/in_events/#", "/SM/out_events/#", "/SM/devconfig"], hostname="localhost")
+        message = json.loads(msg.payload.decode("utf-8"))
+        if message['operation']['metaData']['operation'] == "subscribe_topic":
+            global topics_list
+            device = msg.topic.replace("/SM/devices/", "")
+            print(device)
+
+            topics = message['operation']['payloadData']['value'].split(';')
+            for topic in topics:
+                topic = topic.replace("/#","/.*")
+                if topic in topics_list:
+                    topics_list[re.compile(topic)].append(device)
+                else:
+                    topics_list[re.compile(topic)] = [device]
+
+
+subscribe.callback(on_message, ["/SM/in_events/#", "/SM/out_events/#", "/SM/devconfig", "/SM/devices/#"], hostname="localhost")
 
 
 
